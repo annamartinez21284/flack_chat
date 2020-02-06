0756a8ea27a7b89fa7865901ad62c55ce9ea589c
Anna <annamartinez0212@gmail.com>
2020-02-06 19:53:42 +0800

    completed channel creation and client-side check for existence

--- application.py
+++ application.py
@@ -1,14 +1,14 @@
 import os
 import eventlet
+import json
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
-# Global variable for channel
-channel = None
+
 # Globally initialise path - not sure needed
 socketio = SocketIO(app)
 
@@ -17,9 +17,26 @@ socketio = SocketIO(app)
 def index():
     return render_template("index.html")
 
-@app.route("/chat")
+# Global variable for channels
+channels = []
+
+@app.route("/get_channels", methods = ["GET", "POST"])
+def get_channels():
+
+  return json.dumps(channels)
+
+@app.route("/chat", methods=["GET", "POST"])
 def chat():
-    return render_template("chat.html")
+  # if no display name redirect to "/" - OR DO THIS IN JS
+  if request.method == "POST":
+    new_channel = request.form.get("new_channel_name") # WHY ARE CHANNELS NOT STORED & ADDED??
+    print("THIS IS THE CHANNEL")
+    print(new_channel)
+    channels.append(new_channel)
+  print("THESE ARE THE CHANNELS")
+  print(channels)
+
+  return render_template("chat.html", channels = channels)
 
 @socketio.on("send") # "send" is event to be broadcasted & defined in client-side emit() (1st argument)
 def send(data): # data should include channel & message (2nd argument in {})
--- static/chat.js
+++ static/chat.js
@@ -4,31 +4,80 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // IF USER ALREADY IS ON CHANNEL
 
-        // IF USER SELECTS EXISTING CHANNEL
+        // IF USER SWITCHES / SELECTS EXISTING CHANNEL
 
         // IF USER CREATES NEW CHANNEL
-        var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
-        socket.on('connect', () => {
-          console.log(`Connected to SOCKET ID: ${socket.id}`);
-            });
-        // WHY CAN'T THE ABOVE 4 LINES NOT BE INSIDE .... .onsubmit? ASK...
         document.querySelector('#new_channel').onsubmit = () => {
+          // stop browser default behaviour (submission of form to server), but NOT propagation to DOM (return false; would)
+          //e.preventDefault(); - using return false below instead - seems to make no difference
           // get new Channel name
           const new_channel_name = document.querySelector('#new_channel_name').value;
           console.log(`New channel name: ${new_channel_name}`);
 
-          // check if channel name already extsis
+
+
+          // STEP1: get channel list from server - PROB BETTER ALL SERVER-SIDE SINCE LIST NEEDS FETCHING FROM IT ANYWAY
+          const request2 = new XMLHttpRequest();
+          request2.open('GET', '/get_channels');
+          // callback function: when request completed,
+          request2.onload = () => {
+            // extract list in JSON format from request (returned by get_channels() in application.py)
+            const response = JSON.parse(request2.responseText);
+            console.log(`RESPONSE FROM SERVER IS: ${response}`);
+            // look for channel in returned array
+            if (response.find(function(element) {return element == new_channel_name;}))
+                {
+
+              // empty input field (reset form)
+              document.getElementById('new_channel').reset();
+              // check with server if channel name already exists (AJAX request)
+              alert("Channel already exists"); // this always gets called & then goes on to else
+
+              return false;
+              }
+            else {
+              // send new channel name to server
+              const request1 = new XMLHttpRequest(); // DO I NEED 2 request objects?
+              request1.open('POST', '/chat');
+              const data = new FormData();
+              data.append('new_channel_name', new_channel_name);
+              console.log(`SENDING ${new_channel_name} TO SERVER`);
+              request1.send(data);
+              // create new HTML item for channel list & append
+              const option = document.createElement('option');
+              option.innerHTML = new_channel_name;
+              document.querySelector('select').append(option);
+
+              var socket = io(`/${new_channel_name}`);
+              socket.on('connect', () => {
+                console.log(`CONNECTED TO NAMESPACE:${socket.nsp}`); // or .nsp.name
+
+              });
+              // empty input field (reset form)
+              document.getElementById('new_channel').reset();
+            }
+          };
+          request2.send();
+
+
+
+          // save channel on server
+          //const request3 = new XMLHttpRequest();
+          //request3.open('POST', '/chat');
+          //request3.send(new_channel_name); // this isnt being sent
+
+
+
+
+
           // connect to websocket and tell Socket.IO client to connect to chosen channel name (path)
           //var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port,  {'path': `/${new_channel_name}.io` });// is here the right place where i initialise path url?
-          //var socket = io.of('new_channel_name');
-
-          //var socket = io('/my-namespace');
+          //var socket = io.of('new_channel_name'); // this is server-side code??
+          //var socket = io(`/${new_channel_name}`);
           //var socket = io();
-
+          //var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
           // once, connected, do...
-          socket.on('connect', () => {
-            console.log("THIS NEVER GETS EXECUTED");
-          }); return false;
 
+          return false;
           };
       });
